1561b6b3d56406d119d8c2fd3705f7cfbe30fcb1
m0-dev <moritz.michel@online.de>
2025-12-03 03:32:29 +0100

    feat: add and configure BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Dashboard from './pages/Dashboard'
+import Favorite from './pages/Favorites'
 
 function App() {
   return (
-    <>
-      <Dashboard />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="/favorites" element={<Favorite />}></Route>
+      </Routes>
+    </main>
   )
 }
 
