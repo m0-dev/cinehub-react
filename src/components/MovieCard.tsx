8a46cf7437092c3282c4b06ea2cfee3fa6eb78ae
m0-dev <moritz.michel@online.de>
2025-12-03 02:50:21 +0100

    feat: add MovieCard component

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,25 @@
+function MovieCard({movie}: { movie: { title: string; url: string; release_date: string }}) {
+    function onFavoriteClick() {
+        alert("Favorite Buton clicked")
+    }
+
+    return <div className="movie-card">
+        <div className="movie-image">
+            <img 
+                src={movie.url || `https://picsum.photos/200/300`}
+                alt={movie.title}
+            />
+            <div className="movie-overlay">
+                <button className="favorite-btn" onClick={onFavoriteClick}>
+                    ❤
+                </button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date}</p>
+        </div>
+    </div>
+}
+
+export default MovieCard
